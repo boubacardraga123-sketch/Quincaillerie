64b138fe5f31e15509e9239344e7c6fb6e01400d
boubacardraga123-sketch <boubacardraga123@gmail.com>
2026-08-30 20:03:29 +0000

    Remove icon and application menu from main window
    Remove icon path and default menu for a cleaner app.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+
 const { app, BrowserWindow, Menu } = require('electron');
-const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,7 +7,6 @@ function createWindow() {
     height: 800,
     minWidth: 900,
     minHeight: 600,
-    icon: path.join(__dirname, 'build', 'icon.png'),
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false,
@@ -16,7 +15,6 @@ function createWindow() {
     backgroundColor: '#f0f4f8',
   });
 
-  // Retire le menu par défaut (File/Edit/View...) pour une app plus propre
   Menu.setApplicationMenu(null);
 
   win.loadFile('index.html');
