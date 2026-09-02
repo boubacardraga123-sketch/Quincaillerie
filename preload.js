813abbdfd993867b6cfba5dcbb5f6d5e4a0b51dd
boubacardraga123-sketch <boubacardraga123@gmail.com>
2026-09-02 19:37:45 +0000

    Delete preload.js

--- preload.js
+++
@@ -1,3 +0,0 @@
-// Préchargement — pour l'instant l'app ne nécessite pas d'API Node côté page,
-// ce fichier est prêt si vous voulez exposer des fonctions natives plus tard
-// via contextBridge.exposeInMainWorld().
